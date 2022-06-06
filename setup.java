249e470a11475b2ad82600037119fcefd563fb2d
shadowstar906 <106968214+shadowstar906@users.noreply.github.com>
2022-06-06 14:50:52 +0300

    Update setup.java

--- setup.java
+++ setup.java
@@ -1,5 +1,15 @@
 class Main {
 	public static void main(String[] args) {
+		System.out.println("10%");
+		System.out.println("20%");
+		System.out.println("30%");
+		System.out.println("40%");
+		System.out.println("50%");
+		System.out.println("60%");
+		System.out.println("70%");
+		System.out.println("80%");
+		System.out.println("90%");
+		System.out.println("100%");
 		System.out.println("Thank for setup game Arkanoid! Made by shadowstar906");
 	}
 }
